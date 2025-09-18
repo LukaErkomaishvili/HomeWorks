833db7e1b7a93327d90edf6f2b72cca6eff17139
LukaErkomaishvili <luka.erkomaishvili02@gmail.com>
2025-09-18 15:46:36 +0400

    Homework_Week_5 First Commit

---
+++ Homework_Week_5/Homework_Week_5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_Week_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
